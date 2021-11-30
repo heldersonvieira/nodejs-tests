a525091489ead1fa86d385db161e0eaf0ee866ff
heldersonvieira <vieirahelder.fort@gmail.com>
2021-11-30 20:22:20 -0300

    feat: added unit test for create user

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("Create user", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "user test",
+      email: "test@test.com",
+      password: "1234",
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+});
